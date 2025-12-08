cf2c16dd22dd0fa88227458be0021a67e5a82500
Abel Palmer <abelpalmer22@gmail.com>
2025-12-07 22:44:05 -0700

    changed some code for streamlinedness and readableness

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -11,7 +11,6 @@ import java.util.Collection;
 public class GameService {
     private final GameDAO gameDAO;
     private final AuthDAO authDAO;
-
     public GameService(GameDAO gameDAO, AuthDAO authDAO) {
         this.gameDAO = gameDAO;
         this.authDAO = authDAO;
